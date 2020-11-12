8cadea74a070f33e0818c5506e04d1ae53d2ef04
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-12 15:11:40 -0800

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, "public"
     set :views, "app/views"
+    enable :sessions #because of this and the lower line we will now have access to a hash called session that we can manipulate and which will be the way we log users in and out.
+    set :session_secret, "study_session_tracker_secret"
   end
 
   get "/" do
