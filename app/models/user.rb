e864d3cfffffb5dea25a4536bfb1fd770464c5fb
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-11 22:49:05 -0800

    add commented notes to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  has_secure_password #allows you to use bcrypt to hash and salt your passwords and save them in the database under a column called password_digest
   has_many :sessions
   has_many :study_lists, through: :sessions
 end
+
+#has_secure_password also gives you access to the authenticate method.
