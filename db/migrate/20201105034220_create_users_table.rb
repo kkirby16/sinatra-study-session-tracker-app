2f56a5126e343ed7af1140df25242f4396d7cc07
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-11 19:47:00 -0800

    add timestamps to the user migration file

--- db/migrate/20201105034220_create_users_table.rb
+++ db/migrate/20201105034220_create_users_table.rb
@@ -3,6 +3,8 @@ class CreateUsersTable < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+
+      t.timestamps null: false
     end
   end
 end
