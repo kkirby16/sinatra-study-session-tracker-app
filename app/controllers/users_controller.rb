491df7736b15432f0803533e2c539ff21f672250
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-12 15:12:45 -0800

    code out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController #any methods you build in applicat
     erb :login
   end
 
+  post "/login" do #we're creating a session here... aka we are adding a key/value pair to the session hash.
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password]) #making sure the user has the right credentials.
+      session[:user_id] = user.id #what actually logs the user in.
+      puts session
+      redirect "/index"
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/index" do
+    "this will be the page welcoming a user."
+  end
+
   get "/signup" do
   end
 end
