c7f7055e2f3f6c30e322bed7175997230d2d4cf3
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-20 11:37:39 -0800

    remigrate and reseed the db

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20201112041206) do
 
   create_table "study_lists", force: :cascade do |t|
     t.string   "topic"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
