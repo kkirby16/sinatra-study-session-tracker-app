7e220ef1dca934da465154e50fd8dec1d2ecf9db
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-20 11:07:05 -0800

    make code simpler by taking out validating logic and using more model validations instead, and add edit and delete functionality.

--- app/controllers/study_lists_controller.rb
+++ app/controllers/study_lists_controller.rb
@@ -23,15 +23,18 @@ class StudyListsController < ApplicationController
   end
 
   post "/studylists" do
-    if params[:topic] != "" && current_user.study_lists.find_by(topic: params[:topic]) == nil
-      @user = current_user
-      @studylist = StudyList.create(topic: params[:topic])
-      @user.study_lists << @studylist
+    @user = current_user
+    @studylist = StudyList.new(topic: params[:topic], user_id: @user.id)
+    if @studylist.save
       redirect to "/studylists/#{@studylist.id}"
     else
-      redirect "/users/login"
+      redirect "/studylists/new"
     end
   end
+
+  #@user = User.new(params)
+  #if @user.save
+  #session[:user_id] = @user.id
 
   get "/studylists/:id" do
     if logged_in?
@@ -52,22 +55,52 @@ class StudyListsController < ApplicationController
   end
 
   get "/studylists/:id/sessions/post" do
-    if logged_in?
-      @studylist = StudyList.find_by(id: params[:id])
-      @session = Session.create(time_studied: params[:time_studied], date_completed: params[:date_completed], notes: params[:notes], user_id: current_user.id, study_list_id: @studylist.id)
+    @studylist = StudyList.find_by(id: params[:id])
+    @session = Session.new(time_studied: params[:time_studied], date_completed: params[:date_completed], notes: params[:notes], user_id: current_user.id, study_list_id: @studylist.id)
+    if @session.save
       redirect "/studylists/#{@studylist.id}"
     else
-      redirect "/users/login"
+      redirect "/studylists/#{@studylist.id}/sessions/new"
     end
   end
-
-  #
 
   get "/studylists/:id/sessions/:number" do
     if logged_in?
       @studylist = StudyList.find_by(id: params[:id])
       @session = Session.find_by(id: params[:number])
       erb :'/studylists/session_show'
+    else
+      redirect "/users/login"
+    end
+  end
+
+  get "/studylists/:id/sessions/:number/edit" do
+    @studylist = StudyList.find_by(id: params[:id])
+    @session = Session.find_by(id: params[:number])
+    erb :'/studylists/session_edit'
+  end
+
+  patch "/studylists/:id/sessions/:number" do
+    if logged_in?
+      @studylist = StudyList.find_by(id: params[:id])
+      @session = Session.find_by(id: params[:number])
+      @session.update(time_studied: params[:time_studied], date_completed: params[:date_completed], notes: params[:notes])
+      if @session.save
+        redirect "/studylists/#{@studylist.id}/sessions/#{@session.id}"
+      else
+        redirect "/studylists/#{@studylist.id}/sessions/#{@session.id}/edit"
+      end
+    else
+      redirect "/users/login"
+    end
+  end
+
+  delete "/studylists/:id/sessions/:number/delete" do
+    if logged_in?
+      @studylist = StudyList.find_by(id: params[:id])
+      @tweet = Session.find_by(id: params[:number])
+      @tweet.delete
+      redirect "/studylists/#{@studylist.id}"
     else
       redirect "/users/login"
     end
