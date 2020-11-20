4660382db98622e2b99ebf9dd5148e20ba4f4449
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-20 11:14:19 -0800

    change associations and add validations

--- app/models/study_list.rb
+++ app/models/study_list.rb
@@ -1,4 +1,6 @@
 class StudyList < ActiveRecord::Base
+  belongs_to :user
   has_many :sessions
-  has_many :users, through: :sessions
+  validates :topic, presence: true
+  validates_uniqueness_of :topic, scope: :user_id
 end
