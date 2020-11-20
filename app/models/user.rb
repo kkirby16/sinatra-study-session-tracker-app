167973e88eb80dd119f84a4a7f3d08189891bd2f
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-20 11:15:17 -0800

    change associations and also add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password #allows you to use bcrypt to hash and salt your passwords and save them in the database under a column called password_digest
+  has_many :study_lists
   has_many :sessions
-  has_many :study_lists, through: :sessions
+  validates :username, uniqueness: true
+  validates :username, presence: true
 end
 
 #has_secure_password also gives you access to the authenticate method.
