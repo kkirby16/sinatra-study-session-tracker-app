82193b21e1fc07fd12eda146519421ece14be777
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-20 11:37:08 -0800

    add user_id attribute to studylists migration file

--- db/migrate/20201112041206_create_study_lists.rb
+++ db/migrate/20201112041206_create_study_lists.rb
@@ -2,6 +2,7 @@ class CreateStudyLists < ActiveRecord::Migration
   def change
     create_table :study_lists do |t|
       t.string :topic
+      t.integer :user_id
 
       t.timestamps null: false
     end
