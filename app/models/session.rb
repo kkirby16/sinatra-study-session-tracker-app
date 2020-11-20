df6dc558ea579250a25bde1de851ddc2b92fae30
kkirby16 <kkirby16@my.whitworth.edu>
2020-11-20 11:13:07 -0800

    add validations to session model

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,6 @@
 class Session < ActiveRecord::Base
-  belongs_to :user
   belongs_to :study_list
+  belongs_to :user
+  validates :time_studied, presence: true
+  validates :date_completed, presence: true
 end
