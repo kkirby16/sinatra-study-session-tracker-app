f886f7fceb56ed520c508d6416a5accfee9702d7
kkirby16 <kkirby16@my.whitworth.edu>
2020-12-02 14:17:23 -0800

    add redirect_if_not_logged_in and redirect_if_not_authorized helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,5 +20,18 @@ class ApplicationController < Sinatra::Base
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) #returns nil if you pass in a key value pair that doesn't match.
     end #the first time current user is referenced within scope of application controller, this @current_user variable will get created and assigned if the user is found, otherwise it will still be nil. and that way a subsequent call to @current_user if it is already called won't hit the database again. so this is like a little energy saver.
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/users/login"
+      end
+    end
+
+    def redirect_if_not_authorized
+      if @studylist.user != current_user
+        flash[:message] = "*You are not authorized to do this action."
+        redirect "/users/index"
+      end
+    end
   end
 end
