a6f4a9e1715c69d30fa723db32615a282993295f
kkirby16 <kkirby16@my.whitworth.edu>
2020-12-02 14:46:48 -0800

    take out logged_in? if/else statement from methods and add redirect_if_not_logged_in helper method to methods, and add redirect_if_not_authorized helper method to a few methods

--- app/controllers/study_lists_controller.rb
+++ app/controllers/study_lists_controller.rb
@@ -5,39 +5,30 @@ class StudyListsController < ApplicationController
   use Rack::Flash
 
   get "/studylists/new" do
-    if logged_in?
-      @studylists = StudyList.all
-      @studylist = StudyList.find_by(id: params[:id])
+    redirect_if_not_logged_in
+    @studylists = StudyList.all
+    @studylist = StudyList.find_by(id: params[:id])
 
-      erb :'/studylists/new'
-    else
-      redirect "/users/login"
-    end
+    erb :'/studylists/new'
   end
 
   get "/studylists/index" do
-    if logged_in?
-      @studylists = StudyList.all
-      @studylist = StudyList.find_by(id: params[:id])
-      @user = current_user
+    redirect_if_not_logged_in
+    @studylists = StudyList.all
+    @studylist = StudyList.find_by(id: params[:id])
+    @user = current_user
 
-      erb :'/studylists/index'
-    else
-      redirect "/users/login"
-    end
+    erb :'/studylists/index'
   end
 
   post "/studylists" do
-    if logged_in?
-      @studylist = StudyList.new(topic: params[:topic], user_id: current_user.id)
-      if @studylist.save
-        redirect to "/studylists/#{@studylist.id}"
-      else
-        flash[:message] = "*Make sure you have a topic filled in that is also a topic you haven't used before for a studylist."
-        redirect "/studylists/new"
-      end
+    redirect_if_not_logged_in
+    @studylist = StudyList.new(topic: params[:topic], user_id: current_user.id)
+    if @studylist.save
+      redirect to "/studylists/#{@studylist.id}"
     else
-      redirect "/users/login"
+      flash[:message] = "*Make sure you have a topic filled in that is also a topic you haven't used before for a studylist."
+      redirect "/studylists/new"
     end
   end
 
@@ -46,83 +37,65 @@ class StudyListsController < ApplicationController
   #session[:user_id] = @user.id
 
   get "/studylists/:id" do
-    if logged_in?
-      @studylist = StudyList.find_by(id: params[:id])
-      erb :'/studylists/show'
-    else
-      redirect "/users/login"
-    end
+    redirect_if_not_logged_in
+    @studylist = StudyList.find_by(id: params[:id])
+    erb :'/studylists/show'
   end
 
   get "/studylists/:id/sessions/new" do
-    if logged_in?
-      @studylist = StudyList.find_by(id: params[:id])
-      erb :'/studylists/new_session'
-    else
-      redirect "/users/login"
-    end
+    redirect_if_not_logged_in
+    @studylist = StudyList.find_by(id: params[:id])
+    erb :'/studylists/new_session'
   end
 
   get "/studylists/:id/sessions/post" do
-    if logged_in?
-      @studylist = StudyList.find_by(id: params[:id])
-      @session = Session.new(time_studied: params[:time_studied], date_completed: params[:date_completed], notes: params[:notes], user_id: current_user.id, study_list_id: @studylist.id)
-      if @session.save
-        redirect "/studylists/#{@studylist.id}"
-      else
-        flash[:message] = "*Make sure you have filled out both the time studied box and selected a date for date completed. Notes are optional."
-
-        redirect "/studylists/#{@studylist.id}/sessions/new"
-      end
+    redirect_if_not_logged_in
+    @studylist = StudyList.find_by(id: params[:id])
+    @session = Session.new(time_studied: params[:time_studied], date_completed: params[:date_completed], notes: params[:notes], user_id: current_user.id, study_list_id: @studylist.id)
+    if @session.save
+      redirect "/studylists/#{@studylist.id}"
     else
-      redirect "/users/login"
+      flash[:message] = "*Make sure you have filled out both the time studied box and selected a date for date completed. Notes are optional."
+
+      redirect "/studylists/#{@studylist.id}/sessions/new"
     end
   end
 
   get "/studylists/:id/sessions/:number" do
-    if logged_in?
-      @studylist = StudyList.find_by(id: params[:id])
-      @session = Session.find_by(id: params[:number])
-      erb :'/studylists/session_show'
-    else
-      redirect "/users/login"
-    end
+    redirect_if_not_logged_in
+    @studylist = StudyList.find_by(id: params[:id])
+    @session = Session.find_by(id: params[:number])
+    erb :'/studylists/session_show'
   end
 
   get "/studylists/:id/sessions/:number/edit" do
-    if logged_in?
-      @studylist = StudyList.find_by(id: params[:id])
-      @session = Session.find_by(id: params[:number])
-      erb :'/studylists/session_edit'
-    else
-      redirect "/users/login"
-    end
+    redirect_if_not_logged_in
+    @studylist = StudyList.find_by(id: params[:id])
+    @session = Session.find_by(id: params[:number])
+    redirect_if_not_authorized
+    erb :'/studylists/session_edit'
   end
 
   patch "/studylists/:id/sessions/:number" do
-    if logged_in?
-      @studylist = StudyList.find_by(id: params[:id])
-      @session = Session.find_by(id: params[:number])
-      @session.update(time_studied: params[:time_studied], date_completed: params[:date_completed], notes: params[:notes])
-      if @session.save
-        redirect "/studylists/#{@studylist.id}/sessions/#{@session.id}"
-      else
-        flash[:message] = "*Make sure you have filled out both the time studied box and selected a date for date completed. Notes are optional."
-        redirect "/studylists/#{@studylist.id}/sessions/#{@session.id}/edit"
-      end
+    redirect_if_not_logged_in
+    @studylist = StudyList.find_by(id: params[:id])
+    @session = Session.find_by(id: params[:number])
+    redirect_if_not_authorized
+    @session.update(time_studied: params[:time_studied], date_completed: params[:date_completed], notes: params[:notes])
+    if @session.save
+      redirect "/studylists/#{@studylist.id}/sessions/#{@session.id}"
     else
-      redirect "/users/login"
+      flash[:message] = "*Make sure you have filled out both the time studied box and selected a date for date completed. Notes are optional."
+      redirect "/studylists/#{@studylist.id}/sessions/#{@session.id}/edit"
     end
   end
 
   delete "/studylists/:id/sessions/:number/delete" do
-    if logged_in?
-      @studylist = StudyList.find_by(id: params[:id])
-      @session = Session.find_by(id: params[:number])
-      @session.delete
-      redirect "/studylists/#{@studylist.id}"
-    else
-      redirect "/users/login"
-    end
+    redirect_if_not_logged_in
+    @studylist = StudyList.find_by(id: params[:id])
+    @session = Session.find_by(id: params[:number])
+    redirect_if_not_authorized
+    @session.delete
+    redirect "/studylists/#{@studylist.id}"
   end
 end
